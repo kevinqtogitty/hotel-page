0144cb7f68cd4702f63dd4384e0d25506b40052f
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-27 22:26:12 +0100

    Initial commit

---
+++ src/page_sections/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-text">
+        <h1>DISCOVER GREECE</h1>
+        <p>
+          An oasis for barefoot travelers, digital nomads, and adventurers. Our
+          spaces, people and free-spirited energy create the perfect atmosphere
+          to meet new friends and enjoy the simple (and often best) things in
+          life.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
