c96a3f1059c68ab01cf95d8417572ca76fa23a8c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-28 22:23:29 +0100

    Initial commit

---
+++ src/page_sections/BookOnline.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './BookOnline.css';
+
+const BookOnline = () => {
+  return <section className="book-online-container">BookOnline</section>;
+};
+
+export default BookOnline;
