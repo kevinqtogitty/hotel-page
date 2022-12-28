7c521ebefb06e3422a8bd83a5231c34eb4a00a20
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-28 22:26:54 +0100

    Updated: added ref for intersection observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { MutableRefObject, useRef, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import Navigation from './components/Navigation';
+import About from './page_sections/About';
+import BookOnline from './page_sections/BookOnline';
 import Hero from './page_sections/Hero';
 
 function App() {
+  const ref = useRef<HTMLElement>(null);
+  const observer = new IntersectionObserver((position) =>
+    console.log(position)
+  );
   return (
     <>
       <div className="background" />
-      <Navigation />
+      <Navigation blah={observer} sectionRef={ref} />
       <Hero />
+      <About ref={ref} />
+      <BookOnline />
     </>
   );
 }
