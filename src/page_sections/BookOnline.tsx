22cb26071c855b65d9e87d223eb706f1132011af
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-29 18:30:30 +0100

    Updated: completed section

--- src/page_sections/BookOnline.tsx
+++ src/page_sections/BookOnline.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import inBed from '../assets/pictures/discount/discount_1.webp';
+
 import './BookOnline.css';
 
 const BookOnline = () => {
-  return <section className="book-online-container">BookOnline</section>;
+  return (
+    <section className="book-online-container">
+      <img className="book-online-desktop-img" src={inBed} alt="" />
+      <div className="book-online-text">
+        <h2>15% DISCOUNT ON THE HURON WEBSITE</h2>
+        <img className="book-online-mobile-img" src={inBed} alt="" />
+
+        <p>
+          Book directly through our website and get up to 15% discount and
+          access to city guides curated by local experts.
+        </p>
+        <span>Book now</span>
+      </div>
+    </section>
+  );
 };
 
 export default BookOnline;
