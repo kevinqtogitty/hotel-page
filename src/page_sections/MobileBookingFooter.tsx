0c9de8e8d23370505fb2142bed34fc53fcae2042
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-30 19:07:24 +0100

    Initial commit

---
+++ src/page_sections/MobileBookingFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './MobileBookingFooter.css';
+
+const MobileBookingFooter = () => {
+  return (
+    <section className="mobile-booking-footer-section">
+      <button className="mobile-booking-footer-button">Book now</button>
+    </section>
+  );
+};
+
+export default MobileBookingFooter;
