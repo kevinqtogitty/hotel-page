dcd2f8a97393c7fa32e5a15344edd4cec00d7124
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-30 19:24:34 +0100

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { MutableRefObject, useRef, useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React, { useRef } from 'react';
 import Navigation from './components/Navigation';
 import About from './page_sections/About';
 import BookOnline from './page_sections/BookOnline';
+import FAQ from './page_sections/FAQ';
 import Footer from './page_sections/Footer';
+import GiftShop from './page_sections/GiftShop';
 import Hero from './page_sections/Hero';
 import Magazine from './page_sections/Magazine';
+import MobileBookingFooter from './page_sections/MobileBookingFooter';
+import Rooms from './page_sections/Rooms';
 
 function App() {
   const ref = useRef<HTMLElement>(null);
@@ -20,7 +23,11 @@ function App() {
       <About ref={ref} />
       <BookOnline />
       <Magazine />
+      <GiftShop />
+      <Rooms />
+      <FAQ />
       <Footer />
+      <MobileBookingFooter />
     </>
   );
 }
