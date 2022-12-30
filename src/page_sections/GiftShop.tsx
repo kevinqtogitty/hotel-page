539c25d2ea4278c7b20647edd23a721e7c9d37b9
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-30 19:08:40 +0100

    Initial commit

---
+++ src/page_sections/GiftShop.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import gift1 from '../assets/pictures/gift_shop/gift_1.webp';
+import gift2 from '../assets/pictures/gift_shop/gift_2.webp';
+
+import './GiftShop.css';
+
+const GiftShop = () => {
+  return (
+    <div className="gift-shop-section">
+      <h2 className="gift-shop-h2">THE HURON GIFT SHOP</h2>
+      <article className="gift-shop-article">
+        <div className="gift-shop-img-container">
+          <img className="gift-shop-img" src={gift1} alt="Huron candle gift" />
+          <img className="gift-shop-img" src={gift2} alt="Huron blanket gift" />
+        </div>
+        <div className="gift-shop-text">
+          <p className="gift-shop-p">
+            Do you dream of HURON in your home? The Gift Shop is a selection of
+            items co-created by local design studios and craftsmen we value.
+          </p>
+          <span>Visit the HURON store</span>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default GiftShop;
